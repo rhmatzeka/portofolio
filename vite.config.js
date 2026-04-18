d462941bada7d9c6a490298a4f1dca19ab2bdf11
Rahmat Eka <matsganz@gmail.com>
2026-04-19 06:34:56 +0700

    Simplify vite config to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor'
-            }
-            if (id.includes('framer-motion')) {
-              return 'framer-motion'
-            }
-          }
-        }
-      }
-    },
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 1000
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'framer-motion']
-  }
 })
