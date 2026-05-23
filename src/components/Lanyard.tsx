a02056e9c1e76f24192425282e779e2ac037cd82
Rahmat Eka Satria <matsganz@gmail.com>
2026-05-23 12:31:10 +0700

    Improve mobile lanyard sizing

--- src/components/Lanyard.tsx
+++ src/components/Lanyard.tsx
@@ -16,7 +16,7 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 import * as THREE from 'three'
 import cardGLB from '../assets/lanyard/card.glb'
 import lanyard from '../assets/lanyard/lanyard.png'
-import killuaCard from '../assets/kiluaanimated/ezgif-frame-017.png'
+import killuaCard from '../assets/images/k.jpg'
 import './Lanyard.css'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
@@ -99,11 +99,11 @@ function Band({ maxSpeed = 50, minSpeed = 0, isMobile = false }) {
   const rot = new THREE.Vector3()
   const dir = new THREE.Vector3()
   const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 4, linearDamping: 4 }
-  const cardScale = isMobile ? 1.56 : 2.55
+  const cardScale = isMobile ? 2.08 : 2.55
   const colliderScale = cardScale / 1.05
-  const anchorPosition = isMobile ? [-0.84, 3.22, 0] : [-1.1, 3.48, 0]
-  const jointStep = isMobile ? 0.64 : 0.74
-  const cardStartX = isMobile ? 2.02 : 2.44
+  const anchorPosition = isMobile ? [-1.02, 3.58, 0] : [-1.1, 3.48, 0]
+  const jointStep = isMobile ? 0.72 : 0.74
+  const cardStartX = isMobile ? 2.34 : 2.44
   const { nodes, materials } = useGLTF(cardGLB)
   const texture = useTexture(lanyard)
   const killuaTexture = useTexture(killuaCard)
@@ -206,8 +206,8 @@ function Band({ maxSpeed = 50, minSpeed = 0, isMobile = false }) {
                 metalness={0.16}
               />
             </mesh>
-            <mesh position={[0, 0.52, 0.018]}>
-              <planeGeometry args={[0.7, 0.95]} />
+            <mesh position={[0, 0.48, 0.018]}>
+              <planeGeometry args={[0.7, 0.7]} />
               <meshBasicMaterial
                 map={killuaTexture}
                 transparent
